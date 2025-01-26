1812a02c3ea9e04d2c9c877866366efbf1f2670f
Ronald Kaiser <raios.catodicos@gmail.com>
2025-01-26 18:29:34 -0300

    add missing problem link and complexity analysis

--- 2025-01-26/problem1/idea3.py
+++ 2025-01-26/problem1/idea3.py
@@ -1,2 +1,6 @@
+# problem: https://leetcode.com/problems/find-the-winning-player-in-coin-game/
+# space: O(1) | time: O(1)
+
+
 def winning_player(x, y):
     return "Alice" if min(x, y // 4) % 2 else "Bob"
