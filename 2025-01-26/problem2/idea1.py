a12ba404d4cce3bd983e00ebd87137606f965e1a
Ronald Kaiser <raios.catodicos@gmail.com>
2025-01-26 18:03:31 -0300

    add missing code

---
+++ 2025-01-26/problem2/idea1.py
@@ -0,0 +1,14 @@
+# link: https://leetcode.com/problems/balanced-binary-tree/
+# space: O(1) | time: O(n)
+
+
+def height(root):
+    if root is None: return 0
+    return 1 + max(height(root.left), height(root.right))
+
+
+def is_balanced(root):
+    if root is None: return True
+    return is_balanced(root.left) and \
+           is_balanced(root.right) and \
+           abs(height(root.left) - height(root.right)) <= 1
